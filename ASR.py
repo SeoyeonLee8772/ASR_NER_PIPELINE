303263d7c1f22e06dc9eb68b448c2af5fad5fc88
SeoyeonLee8772 <uyrt52@naver.com>
2021-08-13 15:20:15 +0900

    [commit]

---
+++ ASR.py
@@ -0,0 +1,18 @@
+import yaml
+
+from ASR.inference import inference
+
+
+def test():
+
+    with open('ASR/data/config.yaml') as f:
+        opt = yaml.load(f, Loader=yaml.FullLoader)
+
+    opt['audio_path'] = 'INPUT/test.wav'
+    opt['use_val_data'] = False
+    opt['inference'] = True
+    opt['eval'] = False
+    inference(opt)
+
+if __name__ == '__main__':
+    test()
